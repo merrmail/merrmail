3e23788f1d80924515fd020480d20445c2d433f2
David King Roderos <dk.roderos@gmail.com>
2024-02-09 23:34:06 +0800

    Update StringExtensionsTests

--- src/Domain/Common/StringExtensions.cs
+++ src/Domain/Common/StringExtensions.cs
@@ -2,12 +2,13 @@ namespace Merrsoft.MerrMail.Domain.Common;
 
 public static class StringExtensions
 {
+    // This should also replaces escaping characters
     public static string ToDecodedString(this string data)
     {
         // TODO: Implement decoder
         return data;
     }
-
+    
     public static string ToEncodedString(this string data)
     {
         return data;
--- tests/Domain.UnitTests/CommonTests/StringExtensionsTests.cs
+++ tests/Domain.UnitTests/CommonTests/StringExtensionsTests.cs
@@ -14,7 +14,7 @@ public class StringExtensionsTests
     [InlineData("Test Message", "VGVzdCBNZXNzYWdl")]
     [InlineData("Special Characters: !@#$%^&*()_+", "U3BlY2lhbCBDaGFyYWN0ZXJzOiAhQCMkJV4mKigpXykr")]
     [InlineData("Line1\nLine2", "TGluZTEKTGluZTI=")]
-    [InlineData("Carriage\rReturn", "Q2FycmlhZ2UgXHJSZXR1cm4=")]
+    [InlineData("Carriage\rReturn", "Q2FycmlhZ2VcclJldHVybg==")]
     [InlineData("Double\\Backslash", "RG91YmxlXFxCYWNrc2xhc2g=")] 
     [InlineData("With\\Escaping\\Characters", "V2l0aFxEYXJrZXJnXFxDaGFyYWN0ZXJz")]
     public void ToEncodedString_ShouldEncodeString(string input, string expected)
@@ -32,11 +32,11 @@ public class StringExtensionsTests
     [InlineData("", "")]
     [InlineData("VGVzdCBNZXNzYWdl", "Test Message")]
     [InlineData("U3BlY2lhbCBDaGFyYWN0ZXJzOiAhQCMkJV4mKigpXykr", "Special Characters: !@#$%^&*()_+")]
-    [InlineData("TGluZTEKTGluZTI=", "Line1\nLine2")]
-    [InlineData("Q2FycmlhZ2UgXHJSZXR1cm4=", "Carriage\rReturn")]
-    [InlineData("RG91YmxlXFxCYWNrc2xhc2g=", "Double\\Backslash")]
-    [InlineData("V2l0aFxEYXJrZXJnXFxDaGFyYWN0ZXJz", "With\\Escaping\\Characters")]
-    public void ToDecodedString_ShouldDecodeStringAndRemoveEscapes(string input, string expected)
+    [InlineData("TGluZTEKTGluZTI=", "Line1 Line2")] 
+    [InlineData("Q2FycmlhZ2UgXHJSZXR1cm4=", "Carriage Return")] 
+    [InlineData("RG91YmxlXFxCYWNrc2xhc2g=", "Double Backslash")]
+    [InlineData("V2l0aFxEYXJrZXJnXFxDaGFyYWN0ZXJz", "With Escaping Characters")]
+    public void ToDecodedString_ShouldDecodeStringAndReplaceEscapes(string input, string expected)
     {
         // Act
         var decodedString = input.ToDecodedString();
