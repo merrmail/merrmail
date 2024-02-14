cf4537535a6e104e84ce3ba293828c4d539fc062
David King Roderos <dk.roderos@gmail.com>
2024-02-15 00:16:21 +0800

    Revert EnvironmentVariables from record to class

--- src/Domain/Models/EnvironmentVariables.cs
+++ src/Domain/Models/EnvironmentVariables.cs
@@ -1,7 +1,9 @@
 namespace Merrsoft.MerrMail.Domain.Models;
 
-public record EnvironmentVariables(
-    string OAuthClientCredentialsPath,
-    string AccessTokenPath,
-    string DatabaseConnection,
-    string HostAddress);
+public class EnvironmentVariables
+{
+    public string OAuthClientCredentialsPath { get; set; } = string.Empty;
+    public string AccessTokenPath { get; set; } = string.Empty;
+    public string DatabaseConnection { get; set; } = string.Empty;
+    public string HostAddress { get; set; } = string.Empty;
+}
