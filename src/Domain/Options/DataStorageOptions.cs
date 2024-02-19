6324e0af855048e197da0638158fece1f999a694
David King Roderos <dk.roderos@gmail.com>
2024-02-19 10:33:58 +0800

    Fix naming on DataStorageOptions

--- src/Domain/Options/DataStorageOptions.cs
+++ src/Domain/Options/DataStorageOptions.cs
@@ -5,6 +5,6 @@ namespace Merrsoft.MerrMail.Domain.Options;
 
 public class DataStorageOptions
 {
-    [Required] public required DataStorageType DatabaseType { get; set; }
-    [Required] public required string DatabaseAccess { get; set; }
+    [Required] public required DataStorageType DataStorageType { get; set; }
+    [Required] public required string DataStorageAccess { get; set; }
 }
