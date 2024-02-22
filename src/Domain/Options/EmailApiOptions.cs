2d2e9e6afd007792c93bbc8455e7469ab1ce2652
David King Roderos <dk.roderos@gmail.com>
2024-02-22 22:52:25 +0800

    Add HostPassword on EmailApiOptions

--- src/Domain/Options/EmailApiOptions.cs
+++ src/Domain/Options/EmailApiOptions.cs
@@ -5,4 +5,5 @@ public class EmailApiOptions
     public required string OAuthClientCredentialsFilePath { get; set; }
     public required string AccessTokenDirectoryPath { get; set; }
     public required string HostAddress { get; set; }
+    public required string HostPassword { get; set; }
 }
