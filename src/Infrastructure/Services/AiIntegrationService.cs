ae1080023d2fbbb9c0e93c1f6a7f629a2143ff3a
David King Roderos <dk.roderos@gmail.com>
2024-02-26 12:09:01 +0800

    Resharpened code

--- src/Infrastructure/Services/AiIntegrationService.cs
+++ src/Infrastructure/Services/AiIntegrationService.cs
@@ -14,16 +14,15 @@ public class AiIntegrationService(
 {
     private dynamic? _scope;
     private dynamic? _embed;
-    private string? _fullPath;
     private readonly string _dll = tensorFlowBindingOptions.Value.PythonDllFilePath;
-
-    private readonly string universal_sentence_encoder_path =
-        tensorFlowBindingOptions.Value.UniversalSentenceEncoderDirectoryPath;
 
     // Since _scope is dynamic, this unused variable can be used to check what you can do with the _scope
 #pragma warning disable CS0169 // Field is never used
-    private PyModule? _testScope;
+    private readonly PyModule? _test;
 #pragma warning restore CS0169 // Field is never used
+
+    private readonly string universal_sentence_encoder_path =
+        tensorFlowBindingOptions.Value.UniversalSentenceEncoderDirectoryPath;
 
     public bool Initialize()
     {
@@ -34,8 +33,6 @@ public class AiIntegrationService(
             Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", _dll);
             PythonEngine.Initialize();
             _scope = Py.CreateScope();
-            // PythonEngine.BeginAllowThreads();
-
 
             logger.LogInformation("Python initialized.");
         }
@@ -50,40 +47,27 @@ public class AiIntegrationService(
             logger.LogInformation("Setting up TensorFlow...");
             var directory = Directory.GetCurrentDirectory();
             var relativePath = Path.Combine("..", "Infrastructure", "Scripts");
-            _fullPath = Path.GetFullPath(Path.Combine(directory, relativePath));
+            var fullPath = Path.GetFullPath(Path.Combine(directory, relativePath));
 
-            var init = File.ReadAllText(Path.Combine(_fullPath, "__init__.py"));
-            var tf = File.ReadAllText(Path.Combine(_fullPath, "tensorflow.py"));
+            var init = File.ReadAllText(Path.Combine(fullPath, "__init__.py"));
+            var tf = File.ReadAllText(Path.Combine(fullPath, "tensorflow.py"));
 
-            // PythonEngine.Initialize();
-            // using (Py.GIL())
-            // {
-            _scope.Exec(init);
-            _scope.Exec(tf);
+            _scope!.Exec(init);
+            _scope!.Exec(tf);
 
             logger.LogInformation("Loading Universal Sentence Encoder...");
-            _embed = _scope.load_universal_sentence_encoder(universal_sentence_encoder_path);
-            // // var convertedPath = _universalSentenceEncoder.Replace(@"\", @"\\");
-            // // _scope.Exec($"embed = load_universal_sentence_encoder('{convertedPath}')");
-            // _scope.Exec($"embed = load_universal_sentence_encoder(r'{_universalSentenceEncoder}')");
-            // _scope.Exec($"embed = 1");
-            // _embed = _scope!.Eval("embed");
-            // }
-
-            // PythonEngine.BeginAllowThreads();
+            _embed = _scope!.load_universal_sentence_encoder(universal_sentence_encoder_path);
             logger.LogInformation("Finished setting up TensorFlow.");
-            // PythonEngine.BeginAllowThreads();
         }
         catch (Exception ex)
         {
-            // PythonEngine.BeginAllowThreads();
             logger.LogError("Failed to import required Python libraries: {message}", ex.Message);
             return false;
         }
         finally
         {
-            PythonEngine.BeginAllowThreads();
-            // Console.CancelKeyPress += (_, e) => e.Cancel = true;
+            if (PythonEngine.IsInitialized)
+                PythonEngine.BeginAllowThreads();
         }
 
         return true;
@@ -91,15 +75,8 @@ public class AiIntegrationService(
 
     public float GetSimilarityScore(string first, string second)
     {
-        float cosine;
-        // PythonEngine.Initialize();
-        // using (Py.GIL())
-        // {
         var cosine_similarity = _scope!.calculate_cosine_similarity(_embed, first, second);
-        cosine = cosine_similarity.As<float>();
-        // }
-        // PythonEngine.BeginAllowThreads();
-
+        var cosine = cosine_similarity.As<float>();
         return cosine;
     }
 }
