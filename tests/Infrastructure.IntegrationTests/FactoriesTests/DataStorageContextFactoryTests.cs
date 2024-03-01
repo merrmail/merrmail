9a0e5adbec6a516126dbd31005fe34126bbac736
David King Roderos <dk.roderos@gmail.com>
2024-03-01 18:02:42 +0800

    Add Integration tests on Infrastructure layer

---
+++ tests/Infrastructure.IntegrationTests/FactoriesTests/DataStorageContextFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Merrsoft.MerrMail.Domain.Options;
+using Merrsoft.MerrMail.Domain.Types;
+using Merrsoft.MerrMail.Infrastructure.External;
+using Merrsoft.MerrMail.Infrastructure.Factories;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace Infrastructure.IntegrationTests.FactoriesTests;
+
+public class DataStorageContextFactoryTests
+{
+    [Theory]
+    [InlineData(DataStorageType.Sqlite, typeof(SqliteDataStorageContext))]
+    [InlineData(DataStorageType.Csv, typeof(CsvDataStorageContext))]
+    public void CreateDataStorageContext_WithDifferentTypes_ShouldReturnCorrectType(DataStorageType storageType, Type expectedType)
+    {
+        // Arrange
+        var loggerFactoryMock = new Mock<ILoggerFactory>();
+        
+        loggerFactoryMock.Setup(x => x.CreateLogger(It.IsAny<string>()))
+            .Returns(() => new Mock<ILogger>().Object);
+
+        var optionsMock = new Mock<IOptions<DataStorageOptions>>();
+        optionsMock.Setup(x => x.Value).Returns(new DataStorageOptions { DataStorageType = storageType, DataStorageAccess = string.Empty});
+
+        var factory = new DataStorageContextFactory(loggerFactoryMock.Object, optionsMock.Object);
+
+        // Act
+        var result = factory.CreateDataStorageContext();
+
+        // Assert
+        result.Should().BeOfType(expectedType);
+    }
+}
