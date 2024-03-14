1ea6d71ee88ec107b0662aee4006f5f4ffe45e89
David King Roderos <dk.roderos@gmail.com>
2024-03-15 00:39:04 +0800

    Update tests on TensorFlowEmailAnalyzerService

--- tests/Infrastructure.IntegrationTests/ServicesTests/TensorFlowEmailAnalyzerServiceTests.cs
+++ tests/Infrastructure.IntegrationTests/ServicesTests/TensorFlowEmailAnalyzerServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿using FluentAssertions;
+using Merrsoft.MerrMail.Domain.Models;
 using Merrsoft.MerrMail.Infrastructure.Options;
 using Merrsoft.MerrMail.Infrastructure.Services;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Moq;
 
 namespace Infrastructure.IntegrationTests.ServicesTests;
 
@@ -21,5 +23,26 @@ public class TensorFlowEmailAnalyzerServiceTests
 
         // Assert
         result.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void GetEmailReply_ShouldGetReply()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<TensorFlowEmailAnalyzerService>>();
+        var optionsMock = new Mock<IOptions<EmailAnalyzerOptions>>();
+        optionsMock.Setup(x => x.Value).Returns(new EmailAnalyzerOptions { AcceptanceScore = 0.8f });
+        var service = new TensorFlowEmailAnalyzerService(loggerMock.Object, optionsMock.Object);
+        var emailContexts = new List<EmailContext>
+        {
+            new("Subject1", "Response1"),
+            new("Hello", "Correct!")
+        };
+    
+        // Act
+        var reply = service.GetEmailReply("Hi", emailContexts);
+    
+        // Assert
+        reply.Should().Be("Correct!");
     }
 }
